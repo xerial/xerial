2654ee95746440a2fca1308d07256342b6ed127d
Taro L. Saito <leo@xerial.org>
2012-07-06 21:04:22 +0900

    working on logger

--- src/main/scala/xerial/core/util/Logger.scala
+++ src/main/scala/xerial/core/util/Logger.scala
@@ -7,6 +7,11 @@
 
 package xerial.core.util
 
+/**
+ *
+ *
+ * @author Taro L. Saito
+ */
 object LogLevel {
 
   object OFF extends LogLevel(0)
@@ -18,34 +23,41 @@ object LogLevel {
   object TRACE extends LogLevel(6)
   object ALL extends LogLevel(7)
 
+  val values = Seq(OFF, FATAL, ERROR, WARN, INFO, DEBUG, TRACE, ALL)
+
+  def apply(name: String): LogLevel = {
+    val lv = values.find(name == _.name)
+    if (lv.isEmpty) {
+      Console.err.println("Unknown log level: %s. Use info log level instead." format (name))
+      INFO
+    }
+    else
+      lv.get
+  }
+
 
 }
 
-sealed abstract class LogLevel(order:Int) extends Ordered[LogLevel] {
-  def compare(other:LogLevel) = this.order - other.order
+sealed abstract class LogLevel(order: Int) extends Ordered[LogLevel] {
+  val name = this.getClass.getSimpleName.toLowerCase
+  def compare(other: LogLevel) = this.order - other.order
 }
 
 trait Logging {
 
-  protected val _logger : Logger
+  protected val _logger: Logger = Logger(this.getClass)
 
   import LogLevel._
 
-
   protected def fatal(message: => Any): Boolean = log(FATAL, message)
-
   protected def error(message: => Any): Boolean = log(ERROR, message)
-
   protected def warn(message: => Any): Boolean = log(WARN, message)
-
   protected def info(message: => Any): Boolean = log(INFO, message)
-
   protected def debug(message: => Any): Boolean = log(DEBUG, message)
-
   protected def trace(message: => Any): Boolean = log(TRACE, message)
 
   protected def log(logLevel: LogLevel, message: => Any): Boolean = {
-    if(_logger.isEnabled(logLevel)) {
+    if (_logger.isEnabled(logLevel)) {
       _logger.log(logLevel, message)
       true
     }
@@ -58,10 +70,57 @@ trait Logging {
 
 object Logger {
 
-  def apply(cl:Class[_]) : Logger = {
+  def apply(cl: Class[_]): Logger = {
 
 
+  }
 
+  val rootLoggerName = "_root_"
+  val rootLogger = {
+    val l = new StandardLogger(rootLoggerName)
+    def getDefaultLogLevel: LogLevel = {
+
+      val logLevel = LogLevel(Option(System.getProperty("loglevel")).getOrElse("info"))
+    }
+    l.logLevel = Some(getDefaultLogLevel)
+    l
+  }
+
+  /**
+   * Hold logger instances in weakly referenced hash map to allow releasing instances when necessary
+   */
+  protected val loggerHolder = Cache[String, LogWriter](createLogWriter)
+
+  def apply(cl: Class[_]): LogWriter = getLogWriter(cl)
+
+
+  def getLogWriter(cl: Class[_]): LogWriter = {
+    getLogWriter(cl.getName())
+  }
+
+  /**
+   * Get the logger of the specified name. LogWriter names are
+   * dot-separated list of package names. LogWriter naming should be the same with java package/class naming convention.
+   */
+  def getLogWriter(name: String): LogWriter = {
+    if (name.isEmpty)
+      rootLogger
+    else
+      loggerHolder(name)
+  }
+  private def createLogWriter(name: String): LogWriter = {
+    if (LogConfig.enableColor)
+      new LogWriter(name, new ConsoleLogOutput with ANSIColor)
+    else
+      new LogWriter(name, new ConsoleLogOutput)
+  }
+
+  private def parentName(name: String): String = {
+    val p = name.split("""\.""")
+    if (p.isEmpty)
+      LogWriter.rootLoggerName
+    else
+      p.slice(0, p.length - 1).mkString(".")
   }
 
 }
@@ -72,24 +131,45 @@ object Logger {
  */
 trait Logger {
 
-  private val loggerName = this.getClass.getName
-  private val logLevel : LogLevel
+  protected val name = this.getClass.getName
+  protected var logLevel: LogLevel
 
-  def setLogLevel(logLevel:LogLevel)
+  def isEnabled(targetLogLevel: LogLevel): Boolean = targetLogLevel <= logLevel
 
-  def isEnabled(targetLogLevel:LogLevel) : Boolean = targetLogLevel <= logLevel
-
-  def log(level:LogLevel, message: => Any) : Boolean
+  def log(level: LogLevel, message: => Any): Boolean
 
 }
 
 
-class StandardLogger(name:String, val ) extends Logger {
-  private val logLevel = _
+class StandardLogger(name: String, var logLevel:LogLevel) extends Logger {
 
-  def setLogLevel(logLevel: LogLevel) {}
+  import LogLevel._
 
-  def log(level: LogLevel, message: => Any) = false
+  val colorPrefix = Map[LogLevel, String](
+    ALL -> "",
+    TRACE -> Console.GREEN,
+    DEBUG -> Console.WHITE,
+    INFO -> Console.CYAN,
+    WARN -> Console.YELLOW,
+    ERROR -> Console.MAGENTA,
+    FATAL -> Console.RED,
+    OFF -> "")
+
+
+  def log (level: LogLevel, message: => Any) = {
+    def isMultiLine(str:String) = str.contains("\n")
+
+    val m = {
+      val m = message.toString
+      if (isMultiLine(m))
+        "\n" + m
+      else
+        m
+    }
+
+    Console.err.println("%s[%s] %s%s".format(colorPrefix(level), name, m, Console.RESET))
+    true
+  }
 }
 
 
