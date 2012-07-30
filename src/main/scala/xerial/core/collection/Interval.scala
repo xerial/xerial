09949ca14105fd0212d584eda406b9f6e62b9760
Taro L. Saito <leo@xerial.org>
2012-07-30 14:00:46 +0900

    Put red-black tree into impl package

--- src/main/scala/xerial/core/collection/Interval.scala
+++ src/main/scala/xerial/core/collection/Interval.scala
@@ -7,12 +7,14 @@
 
 package xerial.core.collection
 
+
+
 /**
  * Type class representing intervals
  *
  * @author leo
  */
-trait Interval[A, @specialized(Int, Long) V] {
+trait IntervalOps[A, @specialized(Int, Long) V] {
 
   def start(a:A):V
   def end(a:A):V
@@ -22,26 +24,47 @@ trait Interval[A, @specialized(Int, Long) V] {
   def intersect(a:A, b:A) : Boolean
   def contain(a:A, b:A) : Boolean
   def startIsSmaller(a:A, b:A) : Boolean
+  def endIsSmaller(a:A, b:A) :Boolean
 }
 
 
-trait IntInterval[A] extends Interval[A, Int]{
+trait IntInterval[A] extends IntervalOps[A, Int]{
   def precede(a:A, b:A) : Boolean = end(a) < start(b)
   def follow(a:A, b:A) : Boolean = end(b) < start(a)
-  def intersect(a:A, b:A) : Boolean =  start(a) <= end(b) && start(b) <=  end(a)
+  def intersect(a:A, b:A) : Boolean =  start(a) <= end(b) && start(b) <  end(a)
   def contain(a:A, b:A) : Boolean = start(a) <= start(b) && end(b) <= end(a)
   def startIsSmaller(a:A, b:A) : Boolean = start(a) < start(b)
+  def endIsSmaller(a:A, b:A) :Boolean = end(a) < end(b)
+
 }
 
-trait LongInterval[A] extends Interval[A, Long] {
+trait LongInterval[A] extends IntervalOps[A, Long] {
   def precede(a:A, b:A) : Boolean = end(a) < start(b)
   def follow(a:A, b:A) : Boolean = end(b) < start(a)
-  def intersect(a:A, b:A) : Boolean =  start(a) <= end(b) && start(b) <=  end(a)
+  def intersect(a:A, b:A) : Boolean =  start(a) <= end(b) && start(b) <  end(a)
   def contain(a:A, b:A) : Boolean = start(a) <= start(b) && end(b) <= end(a)
   def startIsSmaller(a:A, b:A) : Boolean = start(a) < start(b)
+  def endIsSmaller(a:A, b:A) : Boolean = end(a) < end(b)
 }
 
 
+/**
+ * Concrete Interval class
+ */
 
+class Interval(val start:Int, val end:Int) {
+  def size = end - start
+  override def toString = "[%d,%d)".format(start, end)
+}
 
+object Interval {
 
+  implicit object IntIntervalOps extends IntInterval[Interval] {
+    def start(a:Interval) = a.start
+    def end(a:Interval) = a.end
+  }
+
+  def apply(s:Int, e:Int) = new Interval(s, e)
+  def point(s:Int) = new Interval(s, s)
+
+}
--- src/main/scala/xerial/core/collection/PrioritySearchTree.scala
+++ src/main/scala/xerial/core/collection/PrioritySearchTree.scala
@@ -8,86 +8,116 @@
 package xerial.core.collection
 
 
+package impl {
+
+abstract class Tree[+A] {
+  def isEmpty: Boolean
+  def isBlack: Boolean = true
+  def elem: A
+  def left: Tree[A]
+  def right: Tree[A]
+}
+
+object Empty extends Tree[Nothing] {
+  def left: Tree[Nothing] = Empty
+  def right: Tree[Nothing] = Empty
+  def isEmpty = true
+  def elem = null.asInstanceOf[Nothing]
+  override def toString = "Empty"
+}
+
+
+abstract class NonEmpty[+A] extends Tree[A] {
+  def isEmpty = false
+}
+
+
+case class RedTree[+A](elem: A, left: Tree[A], right: Tree[A]) extends NonEmpty[A] {
+  override def isBlack: Boolean = false
+}
+case class BlackTree[+A](elem: A, left: Tree[A], right: Tree[A]) extends NonEmpty[A] {
+}
+
+}
+
 
 /**
  * @author leo
  */
 abstract class RedBlackTree[A] {
 
-  protected def op:Interval[A, _]
 
-  private def mkTree(isBlack:Boolean, e:A, l:Tree, r:Tree) : Tree = {
-    if(isBlack)
+  import impl._
+
+  protected def isSmaller(a:A, b:A) : Boolean
+
+
+  private def mkTree[B](isBlack: Boolean, e: B, l: Tree[B], r: Tree[B] ): Tree[B] = {
+    if (isBlack)
       BlackTree(e, l, r)
     else
       RedTree(e, l, r)
   }
 
-  abstract class Tree {
-    def isEmpty : Boolean
-    def isBlack : Boolean = true
-    def elem : A
-    def insert(e:A) : Tree
-
-  }
-  object Empty extends Tree {
-    def isEmpty = true
-    def elem  = null.asInstanceOf[A]
-    def insert(e:A) : Tree = RedTree(e, Empty, Empty)
-  }
-
-  abstract class NonEmpty extends Tree {
-    def left : Tree
-    def right: Tree
-    def isEmpty = false
-    def insert(e:A) : Tree = {
-      if(op.startIsSmaller(e, elem))
-        balanceLeft(isBlack, elem, left.insert(e), right)
-      else
-        balanceRight(isBlack, elem, left, right.insert(e))
+  protected def insert(e:A, t:Tree[A]) : Tree[A] = {
+    t match {
+      case Empty => RedTree(e, Empty, Empty)
+      case _ =>
+        if (isSmaller(e, t.elem))
+          balanceLeft(t.isBlack, t.elem, insert(e, t.left), t.right)
+        else
+          balanceRight(t.isBlack, t.elem, t.left, insert(e, t.right))
     }
   }
-  case class RedTree(elem:A, left:Tree, right:Tree) extends NonEmpty {
-    override def isBlack : Boolean = false
-  }
-  case class BlackTree(elem:A, left:Tree, right:Tree) extends NonEmpty {
+
+
+  protected def balanceLeft[A1 >: A](isBlack: Boolean, e: A, left: Tree[A1], right: Tree[A1]): Tree[A1] = left match {
+    case RedTree (y, RedTree (x, a, b), c) =>
+      RedTree (y, BlackTree (x, a, b), BlackTree (e, c, right) )
+    case RedTree (x, a, RedTree (y, b, c) ) =>
+      RedTree (y, BlackTree (x, a, b), BlackTree (e, c, right) )
+    case _ =>
+      mkTree (isBlack, e, left, right)
   }
 
-  private def balanceLeft(isBlack:Boolean, e:A, left:Tree, right:Tree) : Tree = left match {
-    case RedTree(y, RedTree(x, a, b), c) =>
-      RedTree(y, BlackTree(x, a, b), BlackTree(e, c, right))
-    case RedTree(x, a, RedTree(y, b, c)) =>
-      RedTree(y, BlackTree(x, a, b), BlackTree(e, c, right))
+  protected def balanceRight[A1 >: A](isBlack: Boolean, e: A, left: Tree[A1], right: Tree[A1]): Tree[A1] = right match {
+    case RedTree (z, RedTree (y, b, c), d) =>
+      RedTree (y, BlackTree (e, left, b), BlackTree (z, c, d) )
+    case RedTree (y, b, RedTree (z, c, d) ) =>
+      RedTree (y, BlackTree (e, left, b), BlackTree (z, c, d) )
     case _ =>
-      mkTree(isBlack, e, left, right)
-  }
-
-  private def balanceRight(isBlack:Boolean, e:A, left:Tree, right:Tree) :Tree = right match {
-    case RedTree(z, RedTree(y, b, c), d) =>
-      RedTree(y, BlackTree(e, left, b), BlackTree(z, c, d))
-    case RedTree(y, b, RedTree(z, c, d)) =>
-      RedTree(y, BlackTree(e, left, b), BlackTree(z, c, d))
-    case _ =>
-      mkTree(isBlack, e, left, right)
+      mkTree (isBlack, e, left, right)
   }
 
 }
 
 object PrioritySearchTree {
 
+  def empty[A](implicit iv:IntervalOps[A, _]) = new PrioritySearchTree[A](impl.Empty)
+
 }
 
+/**
+ * Persistent balanced priority search tree
+ * @param root
+ * @param iv
+ * @tparam A
+ */
+class PrioritySearchTree[A](root: impl.Tree[A])(implicit iv: IntervalOps[A, _]) extends RedBlackTree[A] {
+  type pst = PrioritySearchTree[A]
+  private var count = 0
+  protected def isSmaller(a:A, b:A) = iv.startIsSmaller(a, b)
 
-class PrioritySearchTree[A](root:RedBlackTree[A]#Tree)(implicit iv:Interval[A, _]) extends RedBlackTree[A] {
-
-  def op = iv
-
-  def insert(e:A) : PrioritySearchTree[A] = {
-    new PrioritySearchTree(root.insert(e))
+  def +(e:A) : pst = insert(e)
+  def insert(e: A): pst = {
+    count += 1
+    new PrioritySearchTree[A](insert(e, root))
   }
 
+  def size = count
 
-
-
+  override def toString = root.toString
 
 }
+
+
