34b9b2dffead4e88736ac75c229ae29e0d9c9e63
Taro L. Saito <leo@xerial.org>
2012-07-30 18:03:12 +0900

    Add test

---
+++ src/test/scala/xerial/core/collection/PrioritySearchTreeTest.scala
@@ -0,0 +1,31 @@
+//--------------------------------------
+//
+// PrioritySearchTreeTest.scala
+// Since: 2012/07/30 12:53 PM
+//
+//--------------------------------------
+
+package xerial.core.collection
+
+import xerial.core.XerialSpec
+
+/**
+ * @author leo
+ */
+class PrioritySearchTreeTest extends XerialSpec {
+
+  "PrioritySearchTree" should {
+    "insert new nodes" in {
+      var p = PrioritySearchTree.empty[Interval]
+      p += Interval(3, 5)
+      p += Interval(4, 9)
+      p += Interval(4, 12)
+      p += Interval(10, 15)
+      debug(p)
+
+      debug(p.mkString(", "))
+    }
+
+  }
+
+}
