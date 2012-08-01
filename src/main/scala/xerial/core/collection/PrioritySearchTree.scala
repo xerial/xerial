1f726d079147fc8070ad6fbe10e56d93faf5148b
Taro L. Saito <leo@xerial.org>
2012-08-01 11:13:55 +0900

    Extract a generic red-black tree part

--- src/main/scala/xerial/core/collection/PrioritySearchTree.scala
+++ src/main/scala/xerial/core/collection/PrioritySearchTree.scala
@@ -10,66 +10,41 @@ package xerial.core.collection
 import xerial.core.log.Logging
 
 
-package impl {
+object RedBlackTree {
 
-abstract class Tree[+A] {
-  def isBlack: Boolean
-  def left: Tree[A]
-  def right: Tree[A]
-  def iterator: Iterator[A]
-  def key: A = null.asInstanceOf[A]
-  def holder: Holder[A]
-}
+  abstract class Tree[+A] {
+    def isBlack: Boolean
+    def left: Tree[A]
+    def right: Tree[A]
+    def iterator: Iterator[A]
+    def key: A = null.asInstanceOf[A]
+    def holder: Holder[A]
+  }
 
-abstract class Holder[+A] extends Iterable[A] {
-  def elem: A
-  def +[A1 >: A](e: A1): Holder[A1]
-  def iterator: Iterator[A]
-}
+  abstract class Holder[+A] extends Iterable[A] {
+    def elem: A
+    def +[A1 >: A](e: A1): Holder[A1]
+    def iterator: Iterator[A]
+  }
 
-case class Single[+A](elem: A) extends Holder[A] {
-  def +[A1 >: A](e: A1) = Multiple(Seq(elem, e))
-  override def toString = "[%s]".format(elem)
-  def iterator = Iterator.single(elem)
-}
+  case class Single[+A](elem: A) extends Holder[A] {
+    def +[A1 >: A](e: A1) = Multiple(Seq(elem, e))
+    override def toString = "[%s]".format(elem)
+    def iterator = Iterator.single(elem)
+  }
 
-case class Multiple[+A](elems: Seq[A]) extends Holder[A] {
-  require(elems.length > 1, "elems must have more than one element")
-  def elem: A = elems(0)
-  def +[A1 >: A](e: A1) = Multiple(elems :+ e)
-  override def toString = "[%s]".format(elems.mkString(", "))
-  def iterator = elems.iterator
-}
-
-
-object Empty extends Tree[Nothing] {
-  def isBlack = true
-  def left = null
-  def right = null
-  override def toString = "Empty"
-  def holder: Holder[Nothing] = null.asInstanceOf[Holder[Nothing]]
-  def iterator: Iterator[Nothing] = Iterator.empty
-}
-
-}
+  case class Multiple[+A](elems: Seq[A]) extends Holder[A] {
+    require(elems.length > 1, "elems must have more than one element")
+    def elem: A = elems(0)
+    def +[A1 >: A](e: A1) = Multiple(elems :+ e)
+    override def toString = "[%s]".format(elems.mkString(", "))
+    def iterator = elems.iterator
+  }
 
 
-object PrioritySearchTree {
-
-  def empty[A](implicit iv: IntervalOps[A, _]) = new PrioritySearchTree[A](impl.Empty, 0)
-
-}
-
-/**
- * Persistent balanced priority search tree implementation. x-values are maintained in binary search tree, and the y-value of the node in the path from the root to leaves
- * are sorted in the descending order. This property is good for answering 3-sided queries [x1, x2] x [y1, infinity).
- * @param root
- * @param iv
- * @tparam A
- */
-class PrioritySearchTree[A](private val root: impl.Tree[A], override val size: Int)(implicit iv: Point2D[A, _]) extends Iterable[A] with Logging {
-
-  import impl._
+  abstract class Node[+A] extends Tree[A] {
+    def iterator: Iterator[A] = left.iterator ++ holder.iterator ++ right.iterator
+  }
 
   case class RedTree[B](override val key: B, holder: Holder[B], left: Tree[B], right: Tree[B]) extends Node[B] {
     def isBlack = false
@@ -79,17 +54,16 @@ class PrioritySearchTree[A](private val root: impl.Tree[A], override val size: I
     def isBlack = true
   }
 
+  object Empty extends Tree[Nothing] {
+    def isBlack = true
+    def left = null
+    def right = null
+    override def toString = "Empty"
+    def holder: Holder[Nothing] = null.asInstanceOf[Holder[Nothing]]
+    def iterator: Iterator[Nothing] = Iterator.empty
+  }
 
-  type pst = PrioritySearchTree[A]
-
-  def +(e: A): pst = insert(e)
-  def insert(e: A): pst = new PrioritySearchTree[A](insert(e, root), size + 1)
-
-  override def toString = root.toString
-
-  def iterator = root.iterator
-
-  private def mkTree[B](isBlack: Boolean, key: B, h: Holder[B], l: Tree[B], r: Tree[B]): Tree[B] = {
+  def mkTree[A](isBlack: Boolean, key: A, h: Holder[A], l: Tree[A], r: Tree[A]): Tree[A] = {
     if (isBlack)
       BlackTree(key, h, l, r)
     else
@@ -98,10 +72,35 @@ class PrioritySearchTree[A](private val root: impl.Tree[A], override val size: I
 
   //def contains(e:A) : Boolean = root.lookup(e)
 
-  private def blacken(t: Tree[A]): Tree[A] = t match {
+  def blacken[A](t: Tree[A]): Tree[A] = t match {
     case RedTree(k, e, l, r) => BlackTree(k, e, l, r)
     case _ => t
   }
+
+}
+
+import RedBlackTree._
+
+abstract class RedBlackTree[A, Repr](protected val root:Tree[A], override val size:Int) extends Iterable[A] with Logging {
+
+  protected def isSmaller(a:A, b:A) : Boolean
+  protected def update(t:Tree[A], e:A) : Tree[A]
+  /**
+   * Create a new key from the current element and its left/right children. The default is the current element
+   * @param e current element
+   * @param l
+   * @param r
+   * @return
+   */
+  protected def newKey(e:A, l:A, r:A) : A = e
+
+  protected def wrap(t:Tree[A], newSize:Int) : Repr
+
+  def iterator = root.iterator
+
+  override def toString = root.toString
+  def +(e: A): Repr = insert(e)
+  def insert(e:A) : Repr = wrap(insert(e, root), size+1)
 
   protected def insert(e: A, tt: Tree[A]): Tree[A] = {
     def insertTo(t: Tree[A]): Tree[A] = t match {
@@ -109,12 +108,12 @@ class PrioritySearchTree[A](private val root: impl.Tree[A], override val size: I
       case _ =>
         val k = t.key
         val h = t.holder
-        if (iv.xIsSmaller(e, k))
+        if (isSmaller(e, k))
           balanceLeft(t.isBlack, k, h, insertTo(t.left), t.right)
-        else if (iv.xIsSmaller(k, e))
+        else if (isSmaller(k, e))
           balanceRight(t.isBlack, k, h, t.left, insertTo(t.right))
         else
-          mkTree(t.isBlack, iv.yUpperBound(k, e), h + e, t.left, t.right) // e.x == k.x
+          update(t, e) // e.x == k.x
     }
 
     blacken(insertTo(tt))
@@ -138,16 +137,33 @@ class PrioritySearchTree[A](private val root: impl.Tree[A], override val size: I
       mkTree(isBlack, newKey(x, l.key, r.key), xh, l, r)
   }
 
-  private def newKey(c: A, l: A, r: A): A = {
+}
+
+object PrioritySearchTree {
+  def empty[A](implicit iv: IntervalOps[A, _]) = new PrioritySearchTree[A](RedBlackTree.Empty, 0)
+}
+
+
+
+
+/**
+ * Persistent balanced priority search tree implementation. x-values are maintained in binary search tree, and the y-value of the node in the path from the root to leaves
+ * are sorted in the descending order. This property is good for answering 3-sided queries [x1, x2] x [y1, infinity).
+ * @param root
+ * @param iv
+ * @tparam A
+ */
+class PrioritySearchTree[A](root: Tree[A], size: Int)(implicit iv: Point2D[A, _])
+  extends RedBlackTree[A, PrioritySearchTree[A]](root, size) {
+
+  protected def isSmaller(a:A, b:A) : Boolean = iv.xIsSmaller(a, b)
+  protected def update(t:Tree[A], e:A) : Tree[A] = mkTree(t.isBlack, iv.yUpperBound(t.key, e), t.holder + e, t.left, t.right)
+  override protected def newKey(c: A, l: A, r: A): A = {
     def m(k1: A, k2: A): A = Option(k2).map(iv.yUpperBound(k1, _)).getOrElse(k1)
     m(m(c, l), r)
   }
 
-  abstract class Node[+A] extends Tree[A] {
-    def iterator: Iterator[A] = left.iterator ++ holder.iterator ++ right.iterator
-
-  }
-
+  protected def wrap(t:Tree[A], newSize:Int) : PrioritySearchTree[A] = new PrioritySearchTree(t, newSize)
 
 
 }
