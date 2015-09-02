d9685be85bbb949e7bdd728d92c21636f5a553fa
Taro L. Saito <leo@xerial.org>
2015-09-02 16:37:37 -0700

    add pomExtra

--- project/XerialBuild.scala
+++ project/XerialBuild.scala
@@ -47,7 +47,34 @@ object XerialBuild extends Build {
     // Since sbt-0.13.2
     incOptions := incOptions.value.withNameHashing(true),
     crossPaths := false,
-    scalacOptions ++= Seq("-encoding", "UTF-8", "-deprecation", "-unchecked", "-target:jvm-1.6", "-feature")
+    scalacOptions ++= Seq("-encoding", "UTF-8", "-deprecation", "-unchecked", "-target:jvm-1.6", "-feature"),
+    pomExtra := {
+      <url>http://xerial.org/</url>
+        <licenses>
+          <license>
+            <name>Apache 2</name>
+            <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+          </license>
+        </licenses>
+        <scm>
+          <connection>scm:git:github.com/xerial/xerial.git</connection>
+          <developerConnection>scm:git:git@github.com:xerial/xerial.git</developerConnection>
+          <url>github.com/xerial/xerial.git</url>
+        </scm>
+        <properties>
+          <scala.version>
+            {SCALA_VERSION}
+          </scala.version>
+          <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+        </properties>
+        <developers>
+          <developer>
+            <id>leo</id>
+            <name>Taro L. Saito</name>
+            <url>http://xerial.org/leo</url>
+          </developer>
+        </developers>
+    }
   )
 
   import Dependencies._
